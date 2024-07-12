dc2ef565fea87795712598fa12fd77b85fcf4be9
tehseendahya <tdahya2@gmail.com>
2024-07-12 18:52:30 -0400

    new css path

--- pages/post/index.tsx
+++ pages/post/index.tsx
@@ -3,7 +3,8 @@ import { supabase } from '../../daylee/lib/supabase'
 import { useRouter } from 'next/router'
 import { User } from '@supabase/supabase-js'
 import Image from 'next/image'
-import styles from 'pages/post/Post.module.css'
+import styles from './Post.module.css'
+
 
 export default function Post() {
   const [content, setContent] = useState('')
