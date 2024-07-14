af46d5e54858f2144944d1766d4cff6fdcc28b5b
tehseendahya <tdahya2@gmail.com>
2024-07-14 08:49:08 -0400

    changed redirect path

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -19,7 +19,7 @@ export default function Login() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: 'http://localhost:3000'
+        redirectTo: 'https://postdaylee.com'
       },
     });
     if (error) {
